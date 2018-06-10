ff7ab8aa21f0b2360e376386e7eec2ac2cc3858d
Emmanuel <emmanuel.stroem@gmail.com>
2018-06-10 11:24:36 +0800

    Created Pod Lib Project

---
+++ Example/AwesomeDownloading/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AwesomeDownloading
+//
+//  Created by emmanuelstroem on 06/10/2018.
+//  Copyright (c) 2018 emmanuelstroem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
